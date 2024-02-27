11dee051f3f9f48f88566bdde61e8591f97e6f57
AhmedMazroua <126991045+AhmedMazroua@users.noreply.github.com>
2024-02-26 23:53:14 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.fromARGB(255, 166, 0, 255)),
         ),
         home: MyHomePage(),
       ),
